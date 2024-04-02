acf96dd95895a91a07208154319f95fd46367047
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-02 11:45:34 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/itel/itel-L6006
+DEVICE_PATH := device/itel/L6006
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
