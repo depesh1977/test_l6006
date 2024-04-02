ae1be7ee65d1ee6bbfee25493e20afe557670938
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-02 11:44:44 +0300

    Update twrp_L6006.mk

--- twrp_L6006.mk
+++ twrp_L6006.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-#$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 
 PRODUCT_DEVICE := L6006
