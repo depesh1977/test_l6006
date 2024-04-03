2227cae1c1f659ba714df911cfc3388d854f7ff2
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-03 11:19:45 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service \
-    android.hardware.health@1.0-impl
+    android.hardware.health@2.0-impl
 
 # Fastbootd and fastboot HAL
 PRODUCT_PACKAGES += \
