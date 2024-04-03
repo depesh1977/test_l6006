9c7cbd349a92170db3571c2415d06cf5e37bd218
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-03 18:37:13 +0300

    Update omni_L6006.mk

--- omni_L6006.mk
+++ omni_L6006.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from BQru-5745L device
 $(call inherit-product, device/itel/L6006/device.mk)
